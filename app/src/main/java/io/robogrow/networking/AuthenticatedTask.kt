6457a33595f703cf2ded7bee531fbdea4956249d
Sean Robb <tfx@seawavescollective.net>
2020-09-09 15:02:22 -0400

    Add GSON and start working on authenticated responses

---
+++ app/src/main/java/io/robogrow/networking/AuthenticatedTask.kt
@@ -0,0 +1,19 @@
+package io.robogrow.networking
+
+import com.android.volley.toolbox.JsonObjectRequest
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import io.robogrow.VolleySingleton
+
+abstract class AuthenticatedTask<T>(t: T) {
+    abstract var jsonObjectRequest : JsonObjectRequest;
+    fun sendRequest(){
+
+        jsonObjectRequest.headers
+        VolleySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+    }
+
+    fun receiveRequest(payload:String, type:Class<out T>) : T{
+        return Gson().fromJson(payload, object: TypeToken<T>(){}.type)
+    }
+}
